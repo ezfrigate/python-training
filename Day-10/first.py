2ca08a9874f818e37b541ad8fcea6cb6e3fc3b01
Swapnil Bole <swapnilbole119@gmail.com>
2023-10-16 17:59:56 +0530

    [Day-10] Files

---
+++ Day-10/first.py
@@ -0,0 +1,4 @@
+def fun():
+    print("I am in function fun of first.py")
+    
+name = 'john'
---
+++ Day-10/second.py
@@ -0,0 +1,6 @@
+import sys
+sys.path.append('C:/Local/python-training/Day-10/')
+
+import first as f
+f.fun()
+print('name is', f.name)
